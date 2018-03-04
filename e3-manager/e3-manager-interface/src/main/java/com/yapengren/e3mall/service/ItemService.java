88ff162d4f9d0731e39388c6c3ad4e5b02dc6e48
yapengren <404210666@qq.com>
2018-03-04 22:04:05 +0800

    商品列表查询:使用PageHelper5

--- e3-manager/e3-manager-interface/src/main/java/com/yapengren/e3mall/service/ItemService.java
+++ e3-manager/e3-manager-interface/src/main/java/com/yapengren/e3mall/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.yapengren.e3mall.service;
 
+import com.yapengren.e3mall.common.pojo.EasyUIDataGridResult;
 import com.yapengren.e3mall.pojo.TbItem;
 
 /**
@@ -11,4 +12,9 @@ public interface ItemService {
      * 根据商品 id 查询商品信息
      */
     TbItem getItemById(long itemId);
+
+    /**
+     * 查询商品列表
+     */
+    EasyUIDataGridResult getItemList(int page, int rows);
 }
--- e3-manager/e3-manager-service/src/main/java/com/yapengren/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager/e3-manager-service/src/main/java/com/yapengren/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,15 @@
 package com.yapengren.e3mall.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.yapengren.e3mall.common.pojo.EasyUIDataGridResult;
 import com.yapengren.e3mall.mapper.TbItemMapper;
 import com.yapengren.e3mall.pojo.TbItem;
 import com.yapengren.e3mall.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 商品管理 service
@@ -24,5 +29,25 @@ public class ItemServiceImpl implements ItemService {
     public TbItem getItemById(long itemId) {
         TbItem tbItem = tbItemMapper.selectByPrimaryKey(itemId);
         return tbItem;
+    }
+
+    /**
+     * 查询商品列表
+     */
+    @Override
+    public EasyUIDataGridResult getItemList(int page, int rows) {
+        // 设置分页信息
+        PageHelper.startPage(page, rows);
+        // 执行查询
+        List<TbItem> list = tbItemMapper.selectAll();
+        // 创建一个返回值对象
+        EasyUIDataGridResult result = new EasyUIDataGridResult();
+        result.setRows(list);
+        // 取分页结果
+        PageInfo<TbItem> pageInfo = new PageInfo<>(list);
+        // 取总记录数
+        long total = pageInfo.getTotal();
+        result.setTotal(total);
+        return result;
     }
 }
